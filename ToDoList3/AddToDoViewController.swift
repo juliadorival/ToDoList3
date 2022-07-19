11fccc12675b800353e47d6335a11bc3eb443abf
Julia Dorival <julia.dorival20@gmail.com>
2022-07-19 11:50:09 -0700

    Initial Commit

---
+++ ToDoList3/AddToDoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoViewController.swift
+//  ToDoList3
+//
+//  Created by scholar on 7/19/22.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var importantLabel: UILabel!
+    @IBOutlet weak var toDoButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList3/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList3
+//
+//  Created by scholar on 7/19/22.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    
+    var important = false
+}
